2dfeff56858c01270104c6ac4350572fdb3176de
Matheus <cunhamatheusc@gmail.com>
2019-03-26 16:41:42 -0300

    Operadores

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Hello World!");
+console.log(3.1 - 4.1);
+console.log(0.333333333333333333333 * 3);
+console.log(2**4); //Potência
+
+typeof 0//Number
+typeof 1.4 //Number
+
+typeof "foo" //String
+
+typeof true  //Boolean
+typeof false //Boolean
+
+typeof undefined //Undefined
+
+typeof null //Object
