92bb9baf0957a31b552181160ba693750294466e
Matheus <cunhamatheusc@gmail.com>
2019-03-26 17:01:27 -0300

    Strgins

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("Hello World!");
 console.log(3.1 - 4.1);
 console.log(0.333333333333333333333 * 3);
 console.log(2**4); //Potência
+console.log(2>4);
 
 typeof 0//Number
 typeof 1.4 //Number
@@ -14,3 +15,25 @@ typeof false //Boolean
 typeof undefined //Undefined
 
 typeof null //Object
+
+// const -> Variavel constante
+// let -> Variavel dinamica
+
+let mensagem;
+
+mensagem = "Essa é umma 'variável' variável";
+
+console.log(mensagem);
+
+mensagem = "Pode alterar o valor dela";
+
+console.log(mensagem);
+
+/* const mensagem_const; 
+mensagem_const = "Essa é umma 'variável' variável";
+mensagem_const = "Pode alterar o valor dela"; */
+
+let nome = "Matheus";
+let idade = 18;
+
+console.log(`Ele é o ${nome} e tem ${idade} anos`); //Usar " ` " (crase) para interpolar variaveis e strgings ${}
